7fee6700e8dc9ad38f335e0b0d942be5c8291c9f
adamr814 <adamr814@outlook.com>
2023-03-07 11:30:53 -0600

    Beginning of Project 1

---
+++ Project 1/Library.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Scanner;
+
+class book{
+    void AddBook(){};
+
+    String SearchBook(){};
+
+    int InvAudit(){};
+
+    void RemoveBook(){};
+}
+
+private class subscriber{
+
+}
+
+private class Borrow{}
+
+public class main{
+    
+}
