6d19f8673e904d002adc3dd3f59e1f70ebbc5502
adamr814 <adamr814@outlook.com>
2023-03-10 12:30:43 -0600

    12:30p Project 1 Update

--- Project 1/Library.java
+++ Project 1/Library.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 import java.util.Scanner;
 
 class book{
@@ -7,9 +8,10 @@ class book{
     private int ISBN;
     private int Copies;
 
-    public book(String BookTitle, String Author, int copies){
+    public book(String BookTitle, String Author, int ISBN, int copies){
         this.BookTitle = BookTitle;
         this.Author = Author;
+        this.ISBN = ISBN;
         this.Copies = copies;}
 
     //Getters for book class
@@ -17,15 +19,21 @@ class book{
         return this.BookTitle;}
     public String getAuthor(){
         return this.Author;}
+    public int getISBN(){
+        return this.ISBN;}
+    public int getCopies(){
+        return this.Copies;}
+
     //Setters for book class
     public void setBookTitle(String BookTitle){
         this.BookTitle = BookTitle;}
     public void setAuthor(String Author){
         this.Author = Author;}
+    public void setISBN(int ISBN){
+        this.ISBN = ISBN;}
     public void setCopies(int Copies){
-        this.Copies = Copies;}
-    
-}
+        this.Copies = Copies;}}
+
 class subscriber{
     private String FirstName;
     private String LastName;
@@ -43,22 +51,21 @@ class subscriber{
         return this.LastName;}
     public String getAddress(){
         return this.Address;}
+
     //Setters for subscriber class
     public void setFirstName(String FirstName){
         this.FirstName = FirstName;}
     public void setLastName(String LastName){
         this.LastName = LastName;}
     public void setAddress(String Address){
-        this.Address = Address;}
-    }
-}
+        this.Address = Address;}}
 
-//private class Borrow{}
+//class Borrow{}
 
 public class Library{
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-        System.out.print("*****  Library Management System  V0.6  *****\n\n");
+        System.out.print("*****  Library Management System  V0.7  *****\n\n");
         printMenu();
         int choice = 0;
         choice = kb.nextInt();
@@ -67,26 +74,29 @@ public class Library{
                 printBookMenu();
                 choice = kb.nextInt();
                 switch(choice){
-                    case 1:
-                    String BookTitle = kb.next();
-                    String Author = kb.next();
-                    int ISBN = kb.nextInt();
-                    int Copies = kb.nextInt();
-                    book = new book(BookTitle, Author, Copies );
-                    break;
-                    //add book;
+                    case 1: //add book
+                        String BookTitle = kb.next();
+                        String Author = kb.next();
+                        int ISBN = kb.nextInt();
+                        int Copies = kb.nextInt();
+                        book bookObj = new book(BookTitle, Author, ISBN, Copies);
+                        break;
                     
-                    //case 2:
-                    //search for book;
+                    case 2: //search for book;
+                        System.out.print("Search: ");
+                        String Search = kb.next();
+
+                    //case 3: //change details;
+                        //Use the setters to change the details here
+                        //Also use the search function from earlier to look for a certain book
                     
-                    //case 3:
-                    //change details;
-                    
-                    //case 4:
-                    //inventory audit;
-                    
-                    //case 5:
-                    //remove book;
+                    //case 4: //inventory audit;
+                        //use a simple function to get all details from objects and then display them to screen
+
+
+                    //case 5: //remove book;
+                        //use a setter to set all values to null or none
+                        //or find a built in function to delete objects
                     
                     default:
                     System.out.print("Text 2");
@@ -98,27 +108,19 @@ public class Library{
                 choice = kb.nextInt();
                 switch(choice){
                     case 1:
-                        System.out.print("Text 3");
-                        break;
-                    default:
-                        System.out.print("Text 4");
-                        break;
+                    String FirstName = kb.next();
+                    String LastName = kb.next();
+                    String Address = kb.next();
+                    subscriber subObj = new subscriber(FirstName, LastName, Address);
+                    break;
+                    //add subscriber
+                    
+                    //case 2:
+                    //search for subscriber;
+                    
+                    //case 3:
+                    //change details;
                 }}
-                
-        
-
-        //else if(choice == 2){
-            //printSubMenu();}
-
-        //else if(choice == 3){
-            //printBorrowMenu();}
-            
-        //else if(choice == 4){
-            //System.out.print("Exiting Program ");
-            //System.exit(0);}
-        //else{
-            //System.out.print("Invalid Entry");   
-        //}
     kb.close();}
 
 
